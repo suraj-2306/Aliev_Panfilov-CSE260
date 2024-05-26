0149093e825254f46e765285022347874d93d4d9
asas <ashwinagesh@gmail.com>
2024-05-26 13:49:30 -0700

    Scatter equally to all cores. chunks divisible by number of cores.

--- helper.cpp
+++ helper.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * Utilities for the Aliev-Panfilov code
  * Scott B. Baden, UCSD
  * Nov 2, 2015
@@ -8,12 +8,12 @@
 #include <assert.h>
 // Needed for memalign
 #include <malloc.h>
+#include <mpi.h>
 
 using namespace std;
 
 void printMat(const char mesg[], double *E, int m, int n);
-
-
+void printMatNaiveHelper(const char mesg[], double *E, int m, int n);
 
 //
 // Initialization
@@ -23,64 +23,89 @@ void printMat(const char mesg[], double *E, int m, int n);
 // These coordinates are in world (global) coordinate and must
 // be mapped to appropriate local indices when parallelizing the code
 //
-void init (double *E,double *E_prev,double *R,int m,int n){
+void init(double *E, double *E_prev, double *R, int m, int n)
+{
+    // MPI_Init(NULL, NULL);
+
     int i;
 
-    for (i=0; i < (m+2)*(n+2); i++)
-        E_prev[i] = R[i] = 0;
-
-    for (i = (n+2); i < (m+1)*(n+2); i++) {
-	int colIndex = i % (n+2);		// gives the base index (first row's) of the current index
-
-        // Need to compute (n+1)/2 rather than n/2 to work with odd numbers
-	if(colIndex == 0 || colIndex == (n+1) || colIndex < ((n+1)/2+1))
-	    continue;
-
-        E_prev[i] = 1.0;
+    for (i = 0; i < (m + 2) * (n + 2); i++)
+    {
+        R[i] = 0;
+        E_prev[i] = i;
     }
 
-    for (i = 0; i < (m+2)*(n+2); i++) {
-	int rowIndex = i / (n+2);		// gives the current row number in 2D array representation
-	int colIndex = i % (n+2);		// gives the base index (first row's) of the current index
+    // for (i = (n + 2); i < (m + 1) * (n + 2); i++)
+    // {
+    //     int colIndex = i % (n + 2); // gives the base index (first row's) of the current index
+
+    //     // Need to compute (n+1)/2 rather than n/2 to work with odd numbers
+    //     if (colIndex == 0 || colIndex == (n + 1) || colIndex < ((n + 1) / 2 + 1))
+    //         continue;
+
+    //     E_prev[i] = 1.0;
+    // }
+
+    for (i = 0; i < (m + 2) * (n + 2); i++)
+    {
+        int rowIndex = i / (n + 2); // gives the current row number in 2D array representation
+        int colIndex = i % (n + 2); // gives the base index (first row's) of the current index
 
         // Need to compute (m+1)/2 rather than m/2 to work with odd numbers
-	if(colIndex == 0 || colIndex == (n+1) || rowIndex < ((m+1)/2+1))
-	    continue;
+        if (colIndex == 0 || colIndex == (n + 1) || rowIndex < ((m + 1) / 2 + 1))
+            continue;
 
         R[i] = 1.0;
     }
     // We only print the meshes if they are small enough
 #if 1
-    printMat("E_prev",E_prev,m,n);
-    printMat("R",R,m,n);
+    // printMatNaiveHelper("E_prev_initial", E_prev, m + 2, n + 2);
+    // printMat("R",R,m,n);
 #endif
 }
 
-double *alloc1D(int m,int n){
-    int nx=n, ny=m;
+double *alloc1D(int m, int n)
+{
+    int nx = n, ny = m;
     double *E;
     // Ensures that allocatdd memory is aligned on a 16 byte boundary
-    assert(E= (double*) memalign(16, sizeof(double)*nx*ny) );
-    return(E);
+    assert(E = (double *)memalign(16, sizeof(double) * nx * ny));
+    return (E);
 }
 
-void printMat(const char mesg[], double *E, int m, int n){
+void printMat(const char mesg[], double *E, int m, int n)
+{
     int i;
 #if 0
     if (m>8)
       return;
 #else
-    if (m>34)
-      return;
+    if (m > 34)
+        return;
 #endif
-    printf("%s\n",mesg);
-    for (i=0; i < (m+2)*(n+2); i++){
-       int rowIndex = i / (n+2);
-       int colIndex = i % (n+2);
-       if ((colIndex>0) && (colIndex<n+1))
-          if ((rowIndex > 0) && (rowIndex < m+1))
-            printf("%6.3f ", E[i]);
-       if (colIndex == n+1)
-	    printf("\n");
+    printf("%s\n", mesg);
+    for (i = 0; i < (m + 2) * (n + 2); i++)
+    {
+        int rowIndex = i / (n + 2);
+        int colIndex = i % (n + 2);
+        if ((colIndex > 0) && (colIndex < n + 1))
+            if ((rowIndex > 0) && (rowIndex < m + 1))
+                printf("%6.3f ", E[i]);
+        if (colIndex == n + 1)
+            printf("\n");
+    }
+}
+
+void printMatNaiveHelper(const char mesg[], double *E, int m, int n)
+{
+    int i;
+
+    printf("%s\n", mesg);
+    for (i = 0; i < m * n; i++)
+    {
+        int colIndex = i % n;
+        printf("%6.3f ", E[i]);
+        if (colIndex == n - 1)
+            printf("\n");
     }
 }
