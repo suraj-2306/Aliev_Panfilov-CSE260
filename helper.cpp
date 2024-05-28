7510d5061ae603c8e8e2c4a8ba08ce6fbfd02cc7
asas <ashwinagesh@gmail.com>
2024-05-28 04:21:52 -0700

    Malloc whole array in only proc0. Use Scatterv for uneven distribution

--- helper.cpp
+++ helper.cpp
@@ -15,6 +15,15 @@ using namespace std;
 void printMat(const char mesg[], double *E, int m, int n);
 void printMatNaiveHelper(const char mesg[], double *E, int m, int n);
 
+bool isRankMaster(int rank)
+{
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Get the rank of the process
+    if (world_rank == rank)
+        return true;
+    return false;
+}
+
 //
 // Initialization
 //
@@ -25,52 +34,59 @@ void printMatNaiveHelper(const char mesg[], double *E, int m, int n);
 //
 void init(double *E, double *E_prev, double *R, int m, int n)
 {
-    // MPI_Init(NULL, NULL);
-
-    int i;
-
-    for (i = 0; i < (m + 2) * (n + 2); i++)
+    if (isRankMaster(0))
     {
-        R[i] = 0;
-        E_prev[i] = 0;
-    }
+        // MPI_Init(NULL, NULL);
 
-    for (i = (n + 2); i < (m + 1) * (n + 2); i++)
-    {
-        int colIndex = i % (n + 2); // gives the base index (first row's) of the current index
+        int i;
 
-        // Need to compute (n+1)/2 rather than n/2 to work with odd numbers
-        if (colIndex == 0 || colIndex == (n + 1) || colIndex < ((n + 1) / 2 + 1))
-            continue;
+        for (i = 0; i < (m + 2) * (n + 2); i++)
+        {
+            R[i] = 0;
+            E_prev[i] = 0;
+        }
 
-        E_prev[i] = 1.0;
-    }
+        for (i = (n + 2); i < (m + 1) * (n + 2); i++)
+        {
+            int colIndex = i % (n + 2); // gives the base index (first row's) of the current index
 
-    for (i = 0; i < (m + 2) * (n + 2); i++)
-    {
-        int rowIndex = i / (n + 2); // gives the current row number in 2D array representation
-        int colIndex = i % (n + 2); // gives the base index (first row's) of the current index
+            // Need to compute (n+1)/2 rather than n/2 to work with odd numbers
+            if (colIndex == 0 || colIndex == (n + 1) || colIndex < ((n + 1) / 2 + 1))
+                continue;
 
-        // Need to compute (m+1)/2 rather than m/2 to work with odd numbers
-        if (colIndex == 0 || colIndex == (n + 1) || rowIndex < ((m + 1) / 2 + 1))
-            continue;
+            E_prev[i] = 1.0;
+        }
 
-        R[i] = 1.0;
-    }
-    // We only print the meshes if they are small enough
+        for (i = 0; i < (m + 2) * (n + 2); i++)
+        {
+            int rowIndex = i / (n + 2); // gives the current row number in 2D array representation
+            int colIndex = i % (n + 2); // gives the base index (first row's) of the current index
+
+            // Need to compute (m+1)/2 rather than m/2 to work with odd numbers
+            if (colIndex == 0 || colIndex == (n + 1) || rowIndex < ((m + 1) / 2 + 1))
+                continue;
+
+            R[i] = 1.0;
+        }
+        // We only print the meshes if they are small enough
 #if 1
-    // printMatNaiveHelper("E_prev_initial", E_prev, m + 2, n + 2);
-    // printMat("R",R,m,n);
+        // printMatNaiveHelper("E_prev_initial", E_prev, m + 2, n + 2);
+        // printMat("R",R,m,n);
 #endif
+    }
 }
 
 double *alloc1D(int m, int n)
 {
-    int nx = n, ny = m;
-    double *E;
-    // Ensures that allocatdd memory is aligned on a 16 byte boundary
-    assert(E = (double *)memalign(16, sizeof(double) * nx * ny));
-    return (E);
+    if (isRankMaster(0))
+    {
+        int nx = n, ny = m;
+        double *E;
+        // Ensures that allocatdd memory is aligned on a 16 byte boundary
+        assert(E = (double *)memalign(16, sizeof(double) * nx * ny));
+        return (E);
+    }
+    return NULL;
 }
 
 void printMat(const char mesg[], double *E, int m, int n)
--- solve.cpp
+++ solve.cpp
@@ -70,78 +70,35 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
     int world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); // Get the number of processes
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Get the rank of the process
-
-    double *E_prev_rank;
-    double *R_rank;
-
     int smallStride = (m + 2) / world_size;
     int bigStride = smallStride + 1;
     int numBigRanks = (m + 2) % world_size;
     int numSmallRanks = world_size - numBigRanks;
-    int *SmallRanks = (int *)malloc(sizeof(int) * (numSmallRanks));
-    int *BigRanks = (int *)malloc(sizeof(int) * (numBigRanks));
-    double *topRow_rank;
-    double *bottomRow_rank;
+    int stride_rank = (world_rank < numSmallRanks) ? smallStride : bigStride;
+    int *scatterCounts = (int *)malloc(sizeof(int) * world_size);
+    int *sourceOffsets = (int *)malloc(sizeof(int) * world_size);
+    double *E_prev_rank = (double *)malloc(sizeof(double) * (stride_rank + 2) * (n + 2));
+    double *R_rank = (double *)malloc(sizeof(double) * (stride_rank + 2) * (n + 2));
+    double *topRow_rank = (double *)malloc(sizeof(double) * (n + 2));
+    double *bottomRow_rank = (double *)malloc(sizeof(double) * (n + 2));
     int i, j;
 
-    for (i = 0; i < numSmallRanks; i++)
+    for (int i = 0; i < world_size; i++)
     {
-        SmallRanks[i] = i;
+        scatterCounts[i] = ((i < numSmallRanks) ? smallStride : bigStride) * (n + 2);
     }
 
-    for (i = 0; i < numBigRanks; i++)
+    for (i = 0; i < world_size; i++)
     {
-        BigRanks[i] = i + numSmallRanks;
+        sourceOffsets[i] = i * (n + 2) * smallStride + ((i <= numSmallRanks) ? 0 : (n + 2));
     }
 
-    MPI_Group world_group;
-    MPI_Group smallGrp;
-    MPI_Group bigGrp;
-    MPI_Comm small_comm;
-    MPI_Comm big_comm;
-
-    MPI_Comm_group(MPI_COMM_WORLD, &world_group);
-
-    MPI_Group_incl(world_group, numSmallRanks, SmallRanks, &smallGrp);
-    MPI_Comm_create(MPI_COMM_WORLD, smallGrp, &small_comm);
-
-    MPI_Group_incl(world_group, numBigRanks, BigRanks, &bigGrp);
-    MPI_Comm_create(MPI_COMM_WORLD, bigGrp, &big_comm);
-
-    if (world_rank < numSmallRanks)
-    {
-        E_prev_rank = (double *)malloc(sizeof(double) * (smallStride + 2) * (n + 2));
-        R_rank = (double *)malloc(sizeof(double) * (smallStride + 2) * (n + 2));
-    }
-    else
-    {
-        E_prev_rank = (double *)malloc(sizeof(double) * (bigStride + 2) * (n + 2));
-        R_rank = (double *)malloc(sizeof(double) * (bigStride + 2) * (n + 2));
-    }
-
-    topRow_rank = (double *)malloc(sizeof(double) * (n + 2));
-    bottomRow_rank = (double *)malloc(sizeof(double) * (n + 2));
-
-    // Scatter initial condition to all processes. Try to move outside loop.
-    if (world_rank < numSmallRanks)
-    {
-        MPI_Scatter(E_prev, (n + 2) * smallStride, MPI_DOUBLE,
-                    E_prev_rank + (n + 2), (n + 2) * smallStride, MPI_DOUBLE, 0,
-                    small_comm);
-        MPI_Scatter(R, (n + 2) * smallStride, MPI_DOUBLE,
-                    R_rank + (n + 2), (n + 2) * smallStride, MPI_DOUBLE, 0,
-                    small_comm);
-    }
-
-    else
-    {
-        MPI_Scatter((E_prev + numSmallRanks * (n + 2) * smallStride), (n + 2) * bigStride, MPI_DOUBLE,
-                    E_prev_rank + (n + 2), (n + 2) * bigStride, MPI_DOUBLE, 0,
-                    big_comm);
-        MPI_Scatter((R + numSmallRanks * (n + 2) * smallStride), (n + 2) * bigStride, MPI_DOUBLE,
-                    R_rank + (n + 2), (n + 2) * bigStride, MPI_DOUBLE, 0,
-                    big_comm);
-    }
+    MPI_Scatterv(E_prev, scatterCounts, sourceOffsets, MPI_DOUBLE,
+                 E_prev_rank + (n + 2), (n + 2) * stride_rank, MPI_DOUBLE, 0,
+                 MPI_COMM_WORLD);
+    MPI_Scatterv(R, scatterCounts, sourceOffsets, MPI_DOUBLE,
+                 R_rank + (n + 2), (n + 2) * stride_rank, MPI_DOUBLE, 0,
+                 MPI_COMM_WORLD);
 
     for (niter = 0; niter < cb.niters; niter++)
     {
@@ -154,8 +111,6 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
             if (cb.plot_freq)
                 plotter->updatePlot(E, -1, m + 1, n + 1);
         }
-
-        int stride_rank = (world_rank < numSmallRanks) ? smallStride : bigStride;
 
         // Update physical borders
         if (world_rank == 0)
@@ -216,11 +171,11 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
             MPI_Recv(E_prev_rank, (n + 2), MPI_DOUBLE, world_rank - 1, BOTTOM, MPI_COMM_WORLD, MPI_STATUS_IGNORE);                            // Copy into first row
         }
 
-        // printf("\nProcess %d ", world_rank);
-        // if (world_rank < numSmallRanks)
-        //     printMatNaive("E_prev_rank_small", E_prev_rank, smallStride + 2, n + 2);
-        // else
-        //     printMatNaive("E_prev_rank_big", E_prev_rank, bigStride + 2, n + 2);
+        printf("\nProcess %d ", world_rank);
+        if (world_rank < numSmallRanks)
+            printMatNaive("E_prev_rank_small", E_prev_rank, smallStride + 2, n + 2);
+        else
+            printMatNaive("E_prev_rank_big", E_prev_rank, bigStride + 2, n + 2);
 
         // Perform computation
 
@@ -230,18 +185,18 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
 #ifdef FUSED
 
         // Solve for the excitation, a PDE
-        for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j += (n + 2))
-        {
-            E_tmp = E + stride_rank * (n + 2) + j - innerBlockRowStartIndex;
-            E_prev_tmp = E_prev_rank + j;
-            R_tmp = R + stride_rank * (n + 2) + j - innerBlockRowStartIndex;
-            for (i = 0; i < n; i++)
-            {
-                E_tmp[i] = E_prev_tmp[i] + alpha * (E_prev_tmp[i + 1] + E_prev_tmp[i - 1] - 4 * E_prev_tmp[i] + E_prev_tmp[i + (n + 2)] + E_prev_tmp[i - (n + 2)]);
-                E_tmp[i] += -dt * (kk * E_prev_tmp[i] * (E_prev_tmp[i] - a) * (E_prev_tmp[i] - 1) + E_prev_tmp[i] * R_tmp[i]);
-                R_tmp[i] += dt * (epsilon + M1 * R_tmp[i] / (E_prev_tmp[i] + M2)) * (-R_tmp[i] - kk * E_prev_tmp[i] * (E_prev_tmp[i] - b - 1));
-            }
-        }
+        // for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j += (n + 2))
+        // {
+        //     E_tmp = E + stride_rank * (n + 2) + j - innerBlockRowStartIndex;
+        //     E_prev_tmp = E_prev_rank + j;
+        //     R_tmp = R + stride_rank * (n + 2) + j - innerBlockRowStartIndex;
+        //     for (i = 0; i < n; i++)
+        //     {
+        //         E_tmp[i] = E_prev_tmp[i] + alpha * (E_prev_tmp[i + 1] + E_prev_tmp[i - 1] - 4 * E_prev_tmp[i] + E_prev_tmp[i + (n + 2)] + E_prev_tmp[i - (n + 2)]);
+        //         E_tmp[i] += -dt * (kk * E_prev_tmp[i] * (E_prev_tmp[i] - a) * (E_prev_tmp[i] - 1) + E_prev_tmp[i] * R_tmp[i]);
+        //         R_tmp[i] += dt * (epsilon + M1 * R_tmp[i] / (E_prev_tmp[i] + M2)) * (-R_tmp[i] - kk * E_prev_tmp[i] * (E_prev_tmp[i] - b - 1));
+        //     }
+        // }
 #else
         // Solve for the excitation, a PDE
         for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j += (n + 2))
@@ -292,20 +247,22 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
         }
 
         // Swap current and previous meshes
-        double *tmp = E;
-        E = E_prev;
-        E_prev = tmp;
+        // double *tmp = E;
+        // E = E_prev;
+        // E_prev = tmp;
 
     } // end of 'niter' loop at the beginning
 
     //  printMat2("Rank 0 Matrix E_prev", E_prev, m,n);  // return the L2 and infinity norms via in-out parameters
 
-    stats(E_prev, m, n, &Linf, &sumSq);
-    L2 = L2Norm(sumSq);
+    // stats(E_prev, m, n, &Linf, &sumSq);
+    // L2 = L2Norm(sumSq);
 
-    // Swap pointers so we can re-use the arrays
-    *_E = E;
-    *_E_prev = E_prev;
+    // // Swap pointers so we can re-use the arrays
+    // *_E = E;
+    // *_E_prev = E_prev;
+
+    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 void printMat2(const char mesg[], double *E, int m, int n)
@@ -335,10 +292,12 @@ void printMatNaive(const char mesg[], double *E, int m, int n)
 {
     int i;
     int j;
-
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Get the rank of the process
     printf("%s\n", mesg);
     for (i = 0; i < m; i++)
     {
+        printf("Rank%d col%d\t", world_rank, i);
         for (j = 0; j < n; j++)
         {
             printf("%6.3f ", E[i * n + j]);
